fcf9eda0d222d014760cbc6756fa31325188dfa9
model-flat <zhykreg@gmail.com>
2016-05-21 01:00:34 +0300

    Make fake_config_creator fully config-dependent.

--- main/fake_config_creator.py
+++ main/fake_config_creator.py
@@ -1,6 +1,7 @@
 import configparser
 import random
 import struct
+import sys
 
 
 def parse_cfg(filename):
@@ -9,7 +10,9 @@ def parse_cfg(filename):
 
     g = config["General"]
     n = g.getint('electrons count')
+    isbinv = g.getboolean('binary velocities output file')
     velocities_file = g['velocities output file']
+    isbinp = g.getboolean('binary positions output file')
     positions_file = g['positions output file']
 
     pb = config["Positions bounds"]
@@ -24,14 +27,17 @@ def parse_cfg(filename):
     vb_z = (vb['z min'], vb['z max'])
     vb_bounds = vb_x, vb_y, vb_z
 
-    return {"n": n, "pf": positions_file, "pb": pb_bounds, "vf": velocities_file, "vb": vb_bounds}
+    config.clear()
+
+    return {"n": n, "pf": positions_file, "pb": pb_bounds, "vf": velocities_file, "vb": vb_bounds,
+            "isbin_vel": isbinv, "isbin_pos": isbinp}
 
 
 def rnd(a, b):
     return random.random() * (b - a) + a
 
 
-def create_data(n, xrange:tuple, yrange:tuple, zrange:tuple=None):
+def create_data(n, xrange: tuple, yrange: tuple, zrange: tuple = None):
     return [(rnd(xrange[0], xrange[1]),
              rnd(yrange[0], yrange[1]),
              0 if not zrange else rnd(zrange[0], zrange[1])) for i in range(n)]
@@ -47,7 +53,12 @@ def write_file(filename, data, binary=False):
             file.writelines([" ".join([str(d0) for d0 in d]) + "\n" for d in data])
 
 if __name__ == '__main__':
-    config = parse_cfg("../config_creator")
 
-    write_file(config['pf'], create_data(config['n'], *config['pb']))
-    write_file(config['vf'], create_data(config['n'], *config['vb']))
+    if len(sys.argv) < 2:
+        print("too few arguments, exiting...")
+        exit()
+
+    config = parse_cfg(sys.argv[1])
+
+    write_file(config['pf'], create_data(config['n'], *config['pb']), binary=config["isbinp"])
+    write_file(config['vf'], create_data(config['n'], *config['vb']), binary=config["isbinv"])
